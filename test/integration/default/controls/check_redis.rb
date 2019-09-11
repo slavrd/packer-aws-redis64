dedfb6604dffabb087dc4e8640ff647202f4c464
Slav Donchev <slav.donchev@gmail.com>
2019-09-11 15:27:44 +0300

    update tests

--- test/integration/default/controls/check_redis.rb
+++ test/integration/default/controls/check_redis.rb
@@ -9,4 +9,16 @@ control 'check_redis' do
     it {should be_running}
   end
 
+  describe file('/etc/redis/redis.conf') do
+    its('owner') { should eq 'redis'}
+    its('group') { should eq 'redis'}
+  end
+
+  describe port(6379) do
+    it { should be_listening }
+    its('processes') { should include 'redis-server' }
+    its('protocols') { should include 'tcp' }
+    its('addresses') { should include '0.0.0.0' }
+  end
+
 end
